1cd8873b406e3c818e54ed8a88349b2e04b82411
Alexandr Chivarzin <admin@sccraft.ru>
2021-12-28 13:43:13 +0300

    ДЗ (Android)
    Добавлен вывод фотографий животных в AnimalAdapter, который используется в MainActivity

--- ДЗ/AndroidStudioProjects/app/src/main/java/ru/sccraft/bmstulabs/rip/animals/AnimalAdapter.kt
+++ ДЗ/AndroidStudioProjects/app/src/main/java/ru/sccraft/bmstulabs/rip/animals/AnimalAdapter.kt
@@ -1,18 +1,23 @@
 package ru.sccraft.bmstulabs.rip.animals
 
 import android.content.Context
+import android.media.Image
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.ImageView
 import android.widget.TextView
+import com.bumptech.glide.Glide
 
 class AnimalAdapter(context: Context, private val животные: List<Animal>) : BaseAdapter() {
 
     private val lInflater: LayoutInflater
+    private val c: Context
 
     init {
         lInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        c = context
     }
 
     override fun getCount(): Int {
@@ -31,13 +36,16 @@ class AnimalAdapter(context: Context, private val животные: List<Animal>
         // используем созданные, но не используемые view
         var вью: View? = view
         if (вью == null) {
-            вью = lInflater.inflate(android.R.layout.simple_list_item_2, viewGroup, false)
+            вью = lInflater.inflate(R.layout.item_list_animal, viewGroup, false)
         }
         val животное = получить_животное(i)
-        val кличка = вью!!.findViewById<TextView>(android.R.id.text1)
-        val вид = вью.findViewById<TextView>(android.R.id.text2)
+        val кличка = вью!!.findViewById<TextView>(R.id.list_animal_name)
+        val вид = вью.findViewById<TextView>(R.id.list_animal_type)
+        val фото = вью.findViewById<ImageView>(R.id.list_animal_photo)
+
         кличка.text = животное.animal_name
         вид.text = животное.animal_type
+        Glide.with(c).load(животное.animal_photo).into(фото)
         return вью
     }
 
