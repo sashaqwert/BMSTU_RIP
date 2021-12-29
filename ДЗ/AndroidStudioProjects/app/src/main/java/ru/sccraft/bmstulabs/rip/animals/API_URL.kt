7ea4c34322a96ed38fdffde2c8b103e0cbd2af94
Alexandr Chivarzin <admin@sccraft.ru>
2021-12-29 17:34:41 +0300

    ДЗ (Android)
    - Исправлена опечатка в комментарии, обнаруженная при составлении отчёта
    - Мелкая оптимизация кода

--- ДЗ/AndroidStudioProjects/app/src/main/java/ru/sccraft/bmstulabs/rip/animals/API_URL.kt
+++ ДЗ/AndroidStudioProjects/app/src/main/java/ru/sccraft/bmstulabs/rip/animals/API_URL.kt
@@ -3,7 +3,7 @@ package ru.sccraft.bmstulabs.rip.animals
 const val API_URL = "https://dz-rip.pc.sccraft.ru"
 
 /*
-* dz.rip.pc.sccraft.ru - субломен, внесённый в DNS сервер, обслуживающий зону sccraft.ru
+* dz.rip.pc.sccraft.ru - субдомен, внесённый в DNS сервер, обслуживающий зону sccraft.ru
 * На этот суюдлмен был получен SSL сертификат, который требует Android
 * Связь с API осуществляется через IIS HTTP Proxy. Подробнее: https://habr.com/ru/post/240495/
 */
--- ДЗ/AndroidStudioProjects/app/src/main/java/ru/sccraft/bmstulabs/rip/animals/AnimalAdapter.kt
+++ ДЗ/AndroidStudioProjects/app/src/main/java/ru/sccraft/bmstulabs/rip/animals/AnimalAdapter.kt
@@ -1,7 +1,6 @@
 package ru.sccraft.bmstulabs.rip.animals
 
 import android.content.Context
-import android.media.Image
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
